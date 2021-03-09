d928f4e8aecadfeb6ebfa60b15693969e29d1686
Arijit Chaudhury <arijit.chaudhury@gmail.com>
2021-03-09 12:03:34 +0530

    Initial version

---
+++ src/main/java/com/arijit/in/vaultconnection/VaultConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.arijit.in.vaultconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultConnectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arijit/in/vaultconnection/VaultConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arijit.in.vaultconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
